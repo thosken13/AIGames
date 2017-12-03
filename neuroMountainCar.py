1a77d1a5301b0da7b45a8c15ff65b114387ace28
thosken13 <tristanhosken@talktalk.net>
2017-12-03 20:01:13 +0100

    now doesn't start with identical copies of the agent (was stupid mistake)

--- neuroMountainCar.py
+++ neuroMountainCar.py
@@ -10,6 +10,7 @@ class Driver:
         Nhidd = args[1]
         self.brain = Brain.Brain(Nin, Nhidd, 3)
         self.score = 0
+        self.vmax = 0
 
     def action(self, observation):
         self.brain.input(observation)
@@ -20,22 +21,28 @@ class Driver:
 
     def update(self, reward, observation):
         #self.score += reward
-        self.score += abs(observation[1])
+        #self.score += abs(observation[1])
+        if abs(observation[1])>self.vmax:
+            self.vmax = abs(observation[1])
+            self.score = self.vmax
 
     def resurect(self):
         self.score = 0
+        self.vmax = 0
 
-maxGenerations = 5
-NChildren = 3
-Nagents = 6
-agents = [Driver((2,2))]*Nagents
+maxGenerations = 20
+NChildren = 6
+Nagents = 10
+agents = []
+for a in range(Nagents):
+    agents.append(Driver((2,3)))
 scores = np.zeros((maxGenerations, Nagents))
 for gen in range(maxGenerations):
     for i, agent in enumerate(agents):
         mountainCar.play(agent)
         print(agent.score)
         scores[gen,i] = agent.score
-    agents = genetic.populationControl(agents, NChildren, Driver, 2, 2)
+    agents = genetic.populationControl(agents, NChildren, Driver, 2, 3)
     print(gen+1)
     
 x = np.arange(1,maxGenerations+1)
@@ -43,5 +50,7 @@ y = np.mean(scores,1)
 e = np.std(scores,1)
 for i in range(maxGenerations):
     plt.errorbar(x, y, yerr=e)
+plt.xlabel("Generation")
+plt.ylabel("Fitness Score")
 plt.show()
 
