1658bb6c5e756fa6476c4b9d15dbf00a6b41ae5f
thosken13 <tristanhosken@talktalk.net>
2017-12-04 17:19:21 +0100

    runs over a number of episodes

--- discreteQMountainCar/discreteQMountainCar.py
+++ discreteQMountainCar/discreteQMountainCar.py
@@ -1,17 +1,27 @@
 import gym
 import sys
-
 sys.path.append('../')
 import mountainCar
+import QLearnTabular
+import matplotlib.pyplot as plt
+import numpy as np
 
-nState=40
+maxIter = 1000
+nStates=40
+alpha = 0.3
+gamma = 1
+deps = -0.0005
 
-def discretiseState(environment, observation):
-    "discretise the observations so that can use q-learning in tabular form"
-    envMin = environment.observation_space.low
-    envMax = environment.observation_space.high
-    envStep = (envMax - envMin)/nState
-    i = int((observation[0] - envMin[0])/envStep[0])
-    j = int((observation[1] - envMin[1])/envStep[1])
-    return i, j
+x = np.arange(maxIter)
+y = []
 
+environment = gym.make('MountainCar-v0')
+driver = QLearnTabular.QLearnTabular(nStates, environment, alpha, gamma, deps)
+
+for _ in range(maxIter):
+    mountainCar.play(environment, driver)
+    y.append(driver.score)
+    driver.reset()
+
+plt.plot(x,y)
+plt.show()
