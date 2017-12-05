e2cfe0fed5e156e9408798bbd6798a1c8946b0a2
thosken13 <tristanhosken@talktalk.net>
2017-12-05 14:27:07 +0100

    added manual limits for observations 1 and 3 because they re infinite. Also added under and overflow so that don't need to find full range of possibilities. Still need to tweak min and max limits

--- discreteQPoleCart/QLearnTabular.py
+++ discreteQPoleCart/QLearnTabular.py
@@ -1,11 +1,12 @@
 import numpy as np
+import math
+
 
 class QLearnTabular:
-    def __init__(self, nStates, environment, alpha, gamma, deps):
+    def __init__(self, nStates, environment, alpha, gamma, epsilon):
         self.nStates = nStates #number of states per observation per action
         self.env = environment
-        self.epsilon = 0.02
-        self.deps = deps
+        self.epsilon = epsilon
         self.alpha = alpha
         self.gamma = gamma
         self.prevState = 0
@@ -25,36 +26,51 @@ class QLearnTabular:
     def discretiseState(self, observation):
         "discretise the observations so that can use q-learning in tabular form"
         envMin = self.env.observation_space.low
+        envMin[1] = -3.6
+        envMin[3] = -3.6#math.radians(50)
         envMax = self.env.observation_space.high
+        envMax[1] = 3.6
+        envMax[3] = 3.6#math.radians(50)
         envStep = (envMax - envMin)/self.nStates
+        #envStep[1] = round(envStep[1],2)
+        #print("min, max, step", envMin, envMax, envStep)
         s = []
-        for i in range(np.shape(observation)[0]):
-            s.append(int((observation[i] - envMin[i])/envStep[i]))
+        for i in range(np.shape(observation)[0]): 
+            s_ = int((observation[i] - envMin[i])/envStep[i])
+            if s_ < self.nStates:
+                s.append(s_)
+            elif s_ <0:
+                s.append(0)
+                print("underflow at ", i, observation[i])
+            else:
+                s.append(self.nStates-1)
+                print("overflow at ", i, observation[i])
         self.prevState = s
+        #print("obs",observation)
+        #print("state", s)
         return s
 
     def action(self, observation):
         "choose an action based off observation"
-        #s = self.discretiseState(observation)
+        s = self.discretiseState(observation)
         #if np.any(self.qTable[:, s[0],s[1]] == 0):
          #   self.prevAction = np.argmin(self.qTable[s])
         if np.random.rand() < self.epsilon:
             self.prevAction = self.env.action_space.sample()
+            print("epsilon ", self.epsilon)
         else:
-            s = self.discretiseState(observation)
             self.prevAction = np.argmax(self.qTable[tuple(s)])
-            print(self.qTable[tuple(s)])
+            #print(self.qTable[tuple(s)])
         return self.prevAction
             
 
     def update(self, reward, observation):
         "update the Q value table"
-        s = self.discretiseState(observation)
         sPrev = self.prevState
         sPrev.append(self.prevAction)
+        s = self.discretiseState(observation)
         self.qTable[tuple(sPrev)] = (1 - self.alpha)*self.qTable[tuple(sPrev)] + self.alpha*(reward + self.gamma*np.max(self.qTable[tuple(s)]))
         self.score+=reward
-        #self.epsilon += self.deps
 
     def reset(self):
         self.score=0
