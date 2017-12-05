bf960f638c6097c9e6a1f47e350916c2cfeda374
thosken13 <tristanhosken@talktalk.net>
2017-12-05 14:25:00 +0100

    changed learning and exploration rate

--- discreteQPoleCart/discreteQPoleCart.py
+++ discreteQPoleCart/discreteQPoleCart.py
@@ -5,29 +5,37 @@ sys.path.append('../')
 import runEpisode
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import math
 
 maxIter = 10000
-nStates=40
+nStates=8
 alpha = 1
-minAlpha = 0.003
+minAlpha = 0.1
 gamma = 1
-deps = -0.0000
+epsilon = 1
+deps = -0.0003
+mineps = 0.05
 
 x = np.arange(maxIter)
 y = []
 
 environment = gym.make('CartPole-v0')
-agent = QLearnTabular.QLearnTabular(nStates, environment, alpha, gamma, deps)
+agent = QLearnTabular.QLearnTabular(nStates, environment, alpha, gamma, epsilon)
 
 print("Start")
 for i in range(maxIter):
-    runEpisode.play(environment, agent, True)
+    runEpisode.play(environment, agent, False)
     y.append(agent.score)
     agent.reset()
     alph = max(alpha * (0.85 ** (i//100)), minAlpha)
     agent.alpha = alph
+    print("alpha ",alph)
+    #agent.epsilon += deps
+    agent.epsilon = max(min(1, 1.0 - math.log10((i+1)/25)), mineps)
 
-print(agent.qTable)
-print(agent.epsilon)
-plt.plot(x,y)
+data = {"score": y}
+df = pd.DataFrame(data)
+df.rolling(window=int(maxIter/500)).mean()
+plt.plot(x, df.rolling(window=10).mean(), "x")
 plt.show()
