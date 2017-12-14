2aa8bd4594c09c87c40b0d1ce4fb5a755d2b387d
thosken13 <tristanhosken@talktalk.net>
2017-12-14 21:27:41 +0100

    lambda version of q learning

---
+++ discreteQLambdaPoleCart/QLearnLambdaTabular.py
@@ -0,0 +1,71 @@
+import numpy as np
+import math
+
+
+class QLearnTabular:
+    def __init__(self, nStates, environment, alpha, gamma, epsilon, lambd):
+        self.nStates = nStates #number of states per observation per action
+        self.env = environment
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.prevState = 0
+        self.prevAction = 0
+        self.score = 0
+        self.lambd = lambd
+
+        dim = self.env.observation_space.shape[0]
+        shape = []
+        for d in range(3):#dim)
+            shape.append(nStates)
+        shape.append(self.env.action_space.shape[0])
+        self.qTable = np.zeros(shape)
+        self.eligibilityTrace = np.zeros(shape)
+
+    def discretiseState(self, observation):
+        "discretise the observations so that can use q-learning in tabular form"
+        envMin = self.env.observation_space.low
+        envMin[1] = -3.4  #-2.5
+        envMin[3] = -3.3
+        envMax = self.env.observation_space.high
+        envMax[1] = 3.4   #2.5
+        envMax[3] = 3.3
+        envStep = (envMax - envMin)/self.nStates
+        s = []
+        for i in [0,2,3]:#range(np.shape(observation)[0]) 
+            s_ = int((observation[i] - envMin[i])/envStep[i])
+            if s_ < 0:
+                s.append(0)
+                print("underflow at {}, value of {}".format(i, observation[i]))
+            elif s_ < self.nStates:
+                s.append(s_)
+            else:
+                s.append(self.nStates-1)
+                print("overflow at {}, value of {}".format(i, observation[i]))
+        self.prevState = s
+        return s
+
+    def action(self, observation):
+        "choose an action based off Q-Values"
+        s = self.discretiseState(observation)
+        if np.random.rand() < self.epsilon:
+            self.prevAction = self.env.action_space.sample()
+        else:
+            self.prevAction = np.argmax(self.qTable[tuple(s)])
+        return self.prevAction
+
+    def update(self, reward, observation):
+        "update the Q value table"
+        sPrev = self.prevState
+        sPrev.append(self.prevAction)
+        s = self.discretiseState(observation)
+        self.eligibilityTrace[tuple(sPrev)] += 1
+        delta = reward + self.gamma*np.max(self.qTable[tuple(s)]) - self.qTable[tuple(sPrev)]
+        self.qTable += self.alpha*delta*self.eligibilityTrace
+        self.eligibilityTrace = self.lambd*self.gamma*self.eligibilityTrace
+        self.score+=reward
+
+    def reset(self):
+        self.score=0
+
+
---
+++ discreteQLambdaPoleCart/discreteQLambdaPoleCart.py
@@ -0,0 +1,64 @@
+import gym
+import QLearnLambdaTabular
+import sys
+sys.path.append('../')
+import runEpisode
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+
+maxIter = 1000            
+nStates=8 
+alpha = 1 
+minAlpha = 0.05 
+alphaRate = 40   
+gamma = 1       
+epsilon = 1     
+mineps = 0.01   
+epsilonRate = 12  
+lambd = 0.3
+
+x = []
+yscores = []
+yeps = []
+yalpha = []
+
+environment = gym.make('CartPole-v0')
+agent = QLearnLambdaTabular.QLearnTabular(nStates, environment, alpha, gamma, epsilon, lambd)
+streak = 0
+for i in range(maxIter):
+    t = runEpisode.play(environment, agent, False)
+    x.append(i+1)
+    yscores.append(agent.score)
+    yalpha.append(agent.alpha)
+    yeps.append(agent.epsilon)
+    if i+1 >= 100:
+        if sum(yscores[-100:])/100 >= 195:
+            print("Solved after {} episodes!".format(i+1))
+            break
+    agent.reset()
+    #agent.alpha = max(alpha * (0.85 ** (i//alphaRate)), minAlpha)
+    agent.epsilon = max(min(1, 1 - math.log10((i+1)/epsilonRate)), mineps)
+    agent.alpha = agent.epsilon  
+    if i%20 == 0:
+        print("Episode Number {}".format(i))
+
+runEpisode.play(environment, agent, True)
+
+data = {"score": yscores}
+df = pd.DataFrame(data)
+plt.subplot(3,1,1)
+plt.plot(x, df.rolling(window=int(maxIter/500)).mean(), "x")
+plt.ylabel("SMA({}) of score".format(maxIter/500))
+plt.subplot(3,1,2)
+plt.plot(x, yeps)
+plt.ylim((0,1))
+plt.ylabel("exploration rate")
+plt.subplot(3,1,3)
+plt.plot(x, yalpha)
+plt.ylim((0,1))
+plt.plot(x, np.ones_like(x)*minAlpha)
+plt.xlabel("episode")
+plt.ylabel("learning rate")
+plt.show()
