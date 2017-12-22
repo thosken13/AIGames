32a56f2e885640d2006b88495d172fca48d1dcc7
thosken13 <tristanhosken@talktalk.net>
2017-12-22 13:15:34 +0100

    fixed lr for ADAM optimizer

--- lunarLander/deepQNet/deepQNet.py
+++ lunarLander/deepQNet/deepQNet.py
@@ -9,15 +9,15 @@ import pandas as pd
 import math
 
 maxIter = 1000
-alpha = 1 
-minAlpha = 0.05 
-alphaRate = 35  
+alpha = 0.001 #constant for ADAM optimizer (decay built in)
+#minAlpha = 0.05 
+#alphaRate = 35  
 gamma = 0.98     
 epsilon = 1     
 mineps = 0.01   
 epsilonRate = 1000
 hiddenNodes = 30
-batchSize = 1000
+batchSize = 100 #roughly one per episode
 dropOutKeepProb = 0.5
 
 x = []
@@ -42,7 +42,7 @@ for i in range(maxIter):
     agent.reset()
     #agent.alpha = max(alpha * (0.85 ** (i//alphaRate)), minAlpha)
     agent.epsilon = max(min(1, 1 - math.log10((i+1)/epsilonRate)), mineps)
-    agent.alpha = agent.epsilon
+    #agent.alpha = agent.epsilon
     if i%20 == 0:
         print("Episode Number {}".format(i))
     agent.equateWeights()
