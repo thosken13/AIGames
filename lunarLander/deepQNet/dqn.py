5e120e71e9b9c15cb2a626751ce838df5029afa7
thosken13 <tristanhosken@talktalk.net>
2017-12-22 14:24:46 +0100

    fixed shape of observation fed to qApprox in action

--- lunarLander/deepQNet/dqn.py
+++ lunarLander/deepQNet/dqn.py
@@ -65,7 +65,7 @@ class dqn:
         if np.random.rand() < self.epsilon:
             self.prevAction = self.env.action_space.sample()
         else:
-            self.prevAction = np.argmax(self.qApproxNet(observation))
+            self.prevAction = np.argmax(self.qApproxNet(np.reshape(observation, (1,8))))
         return self.prevAction
     
     def train(self):
