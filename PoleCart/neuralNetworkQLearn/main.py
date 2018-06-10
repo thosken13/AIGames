e149b081074f5a1766440aa50e78365818b689fb
thosken13 <tristanhosken@talktalk.net>
2018-06-10 12:36:16 +0100

    Added tests for random seeds in gym

--- PoleCart/neuralNetworkQLearn/main.py
+++ PoleCart/neuralNetworkQLearn/main.py
@@ -2,14 +2,22 @@ import gym
 import sys
 sys.path.append('../../')
 import runEpisode
-import agent as nnAgent
+#import agent as nnAgent
 
-env = gym.make('CartPole-v0')
-env.seed(0)
-print("obs space", env.observation_space.shape)
-print("act space", env.action_space.shape)
-for i in range(5):
-    print(env.action_space.sample())
+def testAndExperiment():
+    env = gym.make('CartPole-v0')
+    env.seed(0)
+    print("obs space", env.observation_space.shape)
+    print("act space", env.action_space.shape)
+    #test random seed of environment is fixed correctly by env.seed()
+    for i in range(5):
+        print(env.reset())
+        env.seed(0)
+    for i in range(5):
+        print(env.action_space.sample()) #spaces are seeded separately and apparently by a fixed seed
+    #agent = nnAgent.NNAgent()
+    #agent.test()
 
-agent = nnAgent.NNAgent()
-agent.test()
+
+
+testAndExperiment()
