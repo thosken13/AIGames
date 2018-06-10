a7f6cce31d05235f3029e1af916130bd3fcdbc8b
thosken13 <tristanhosken@talktalk.net>
2018-06-10 11:05:43 +0100

    confirmed: set random seed for environment

--- PoleCart/neuralNetworkQLearn/main.py
+++ PoleCart/neuralNetworkQLearn/main.py
@@ -8,6 +8,8 @@ env = gym.make('CartPole-v0')
 env.seed(0)
 print("obs space", env.observation_space.shape)
 print("act space", env.action_space.shape)
+for i in range(5):
+    print(env.action_space.sample())
 
 agent = nnAgent.NNAgent()
 agent.test()
