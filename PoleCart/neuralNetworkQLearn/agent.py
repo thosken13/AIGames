9c2fcb350bfa9efab59bfd4f0b3b54aba1b9a021
thosken13 <tristanhosken@talktalk.net>
2018-06-10 10:44:30 +0100

    changed tensorboar to allow different shows show. changed summaries to show all with easy code

--- PoleCart/neuralNetworkQLearn/agent.py
+++ PoleCart/neuralNetworkQLearn/agent.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import time
 import numpy as np
+import os
 
 class NNAgent:
     def __init__(self, alpha=0.01, gamma=0.99, epsilonDecay=0.99, 
-                 nNeuronsHidLayers=[5,5,5]):
+                 nNeuronsHidLayers=[50,50,50]):
         self.learnRate=alpha
         self.gamma=gamma
         self.epsilon=1
@@ -29,22 +30,34 @@ class NNAgent:
             optimizer = tf.train.AdamOptimizer(learning_rate=learnRate).minimize(cost)
         with tf.name_scope("summaries"):
             score = tf.placeholder(tf.float32, name="score")
-            for i, l in enumerate(layers[1:]):
-                tf.summary.histogram("layer "+str(i+1), l)
+            #for i, l in enumerate(layers[1:]):
+            #    tf.summary.histogram("layer "+str(i+1), l)
             tf.summary.scalar("score ", score)
             tf.summary.scalar("cost", cost)
+            for var in tf.trainable_variables():
+                tf.summary.histogram(var.name, var)
         summary = tf.summary.merge_all()
             
         init = tf.global_variables_initializer()
         self.session = tf.Session()
+        tf.set_random_seed(1234)
         self.session.run(init)
-        summaryWriter = tf.summary.FileWriter("tensorboardFiles"+str(time.time()), graph=tf.get_default_graph())
+        summaryWriter = tf.summary.FileWriter("tensorboard/"+self.newTBDir(), graph=tf.get_default_graph())
                 
         netDict = {"in": inputLayer, "out": layers[-1], "target": target, 
                    "score": score,
                    "optimizer": optimizer, "learningRate": learnRate, 
                    "summaryWriter": summaryWriter, "summary": summary}
         return netDict
+        
+    def newTBDir(self):
+        "Produce name for new tensorboard run directory"
+        files = os.listdir("tensorboard/")
+        lastRunN=0
+        for f in files:
+            if int(f[-1]) > lastRunN:
+                lastRunN = int(f[-1])
+        return "run"+str(int(lastRunN)+1)
         
     def writeSummary(self, feedDict):
         summaryString = self.session.run(self.netDict["summary"], feed_dict=feedDict)
@@ -54,12 +67,12 @@ class NNAgent:
     def test(self):
         x = np.reshape(np.array([1,2,3,4]), (1,4))
         y = np.reshape(np.array([5,6]), (1,2))
-        opt, summaryString = self.session.run([self.netDict["optimizer"], self.netDict["summary"]], 
+        summaryString, opt = self.session.run([self.netDict["summary"], self.netDict["optimizer"]], 
                                                 feed_dict={self.netDict["in"]: x, self.netDict["target"]: y, 
                                                            self.netDict["score"]: 5, self.netDict["learningRate"]: 0.01})
         print(opt)
         self.netDict["summaryWriter"].add_summary(summaryString, self.steps)
-        
+        self.netDict["summaryWriter"].flush()
         
         
         
