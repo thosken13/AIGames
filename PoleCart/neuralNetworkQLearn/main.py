ba0376df4972055d132d23a73b2fb7e58915aa5b
thosken13 <tristanhosken@talktalk.net>
2018-06-10 15:11:26 +0100

    Test action function

--- PoleCart/neuralNetworkQLearn/main.py
+++ PoleCart/neuralNetworkQLearn/main.py
@@ -10,14 +10,15 @@ def testAndExperiment():
     print("obs space", env.observation_space.shape)
     print("act space", env.action_space.shape)
     #test random seed of environment is fixed correctly by env.seed()
-    for i in range(5):
-        print(env.reset())
-        env.seed(0)
-    for i in range(5):
-        print(env.action_space.sample()) #spaces are seeded separately and apparently by a fixed seed
+    # for i in range(5):
+    #     print(env.reset())
+    #     env.seed(0)
+    # for i in range(5):
+    #     print(env.action_space.sample()) #spaces are seeded separately and apparently by a fixed seed
     agent = nnAgent.NNAgent(env)
     #agent.test()
-    agent.testNpSeed()
+    # agent.testNpSeed()
+    print(agent.action([1,2,3,4]))
 
 
 
