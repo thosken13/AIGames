c1973e8a0f416a6321fd38dd15ab1fdb6d5c86a8
thosken13 <tristanhosken@talktalk.net>
2018-06-27 11:00:40 +0100

    Can save and restore model and agent

--- PoleCart/neuralNetworkQLearn/agent.py
+++ PoleCart/neuralNetworkQLearn/agent.py
@@ -3,9 +3,11 @@ import time
 import numpy as np
 import random
 import os
+import sys
+import pickle
 
 class NNAgent:
-    def __init__(self, environment, tensorboardDir="tensorboard/", alpha=0.001, gamma=0.99, epsilonDecay=0.995,
+    def __init__(self, environment, tensorboardDir="tensorboard/", modelsDir="models/", agentsDir="agents/", alpha=0.001, gamma=0.99, epsilonDecay=0.995,
                  nNeuronsHidLayers=[10,10,10], batchSize=3, minExp=100):
         #RL parameters
         self.learnRate=alpha
@@ -14,6 +16,9 @@ class NNAgent:
         #model and training stuff
         self.nNeuronsHidLayers = nNeuronsHidLayers
         self.tensorboardDir=tensorboardDir
+        self.modelsDir=modelsDir
+        self.agentsDir=agentsDir
+        self.runNum=self.newRunNum()
         self.netDict=self.buildModel()
         self.batchSize=batchSize
         self.minExp = minExp
@@ -33,7 +38,7 @@ class NNAgent:
     def buildModel(self):
         tf.set_random_seed(1234)
         with tf.name_scope("nn"):
-            inputLayer = tf.placeholder(shape=[None, 4], dtype=tf.float32) #4 observations in polecart
+            inputLayer = tf.placeholder(shape=[None, 4], dtype=tf.float32, name="inputLayer") #4 observations in polecart
             layers = [inputLayer]
             for i, n in enumerate(self.nNeuronsHidLayers):
                 hidLayer = tf.layers.dense(layers[-1], n, activation=tf.nn.relu, name="hiddenlayer"+str(i+1))#try other activations
@@ -44,7 +49,7 @@ class NNAgent:
             target = tf.placeholder(shape=[None, 2], dtype=tf.float32, name="target")
             cost = tf.losses.mean_squared_error(target, out) #try alternatives?
             learnRate = tf.placeholder(tf.float32, name="learningRate")
-            optimizer = tf.train.AdamOptimizer(learning_rate=learnRate).minimize(cost)
+            optimizer = tf.train.AdamOptimizer(learning_rate=learnRate, name="adamOpt").minimize(cost)
         with tf.name_scope("summaries"):
             score = tf.placeholder(tf.float32, name="score")
             tf.summary.scalar("score ", score)
@@ -57,25 +62,93 @@ class NNAgent:
                 tf.summary.histogram(var.name, var)
         summary = tf.summary.merge_all()
 
+        saver = tf.train.Saver()
+
         init = tf.global_variables_initializer()
         self.session = tf.Session()
         self.session.run(init)
-        summaryWriter = tf.summary.FileWriter(self.tensorboardDir+self.newTBDir(), graph=tf.get_default_graph())
+        summaryWriter = tf.summary.FileWriter(self.tensorboardDir+self.runNum, graph=tf.get_default_graph())
 
         netDict = {"in": inputLayer, "out": layers[-1], "target": target,
                    "score": score, "epsilon": eps, "episodes": episodes,
                    "optimizer": optimizer, "learningRate": learnRate,
-                   "summaryWriter": summaryWriter, "summary": summary}
+                   "summaryWriter": summaryWriter, "summary": summary,
+                   "saver": saver}
         return netDict
 
-    def newTBDir(self):
-        "Produce name for new tensorboard run directory (next run number)."
-        files = os.listdir(self.tensorboardDir)
-        lastRunN=0
-        for f in files:
-            if int(f[3:]) > lastRunN:
-                lastRunN = int(f[3:])
-        return "run"+str(int(lastRunN)+1)
+    def newRunNum(self):
+        "Produce name for new tensorboard run directory and model directory(next run number)."
+        #check tensorboard files directory
+        tbFiles = os.listdir(self.tensorboardDir)
+        tbLastRunN=0
+        for f in tbFiles:
+            if int(f[3:]) > tbLastRunN:
+                tbLastRunN = int(f[3:])
+        #check model files directory
+        modelFiles = os.listdir(self.modelsDir)
+        modelLastRunN=0
+        for f in modelFiles:
+            if int(f[3:]) > modelLastRunN:
+                modelLastRunN = int(f[3:])
+        #check agents files directory
+        agentFiles = os.listdir(self.agentsDir)
+        agentLastRunN=0
+        for f in agentFiles:
+            if int(f[3:]) > agentLastRunN:
+                agentLastRunN = int(f[3:])
+        #check have same run number from tensorboard and models
+        if tbLastRunN != modelLastRunN:
+            sys.exit("""New run number does not match between model files and tensorbaord files.
+                        Model (tensoboard) files may have been deleted while the associated tensorbaord (model) file was not.""")
+        if modelLastRunN != agentLastRunN:
+            print(modelLastRunN, agentLastRunN)
+            sys.exit("""New run number does not match between model files and agent files.
+                        Model (agent) files may have been deleted while the associated agent (model) file was not.""")
+        return "run"+str(modelLastRunN+1)
+
+    def save(self):
+        """
+            Save the model and the whole agent. Want to save the agent as well as model because of hyper parameters and scores that are kept in the object.
+        """
+        #save session then close and remove session and other tf objects from this agent object (issues with pickling session and tf objects)
+        self.netDict["saver"].save(self.session, self.modelsDir+self.runNum+"/model", global_step=self.steps)
+        self.session.close()
+        self.session=None
+        self.netDict={}
+        pickleOut = open(self.agentsDir+self.runNum ,"wb")
+        pickle.dump(self, pickleOut)
+        pickleOut.close()
+        print("Saved")
+
+    def restore(self):
+        """
+            Restore the model, the agent must be restored outside of the object, from its pickle (before restoring the model).
+        """
+        tf.reset_default_graph() #clean up
+        self.session = tf.Session()
+        latest_checkpoint = tf.train.latest_checkpoint(self.modelsDir+self.runNum)
+        # Load latest checkpoint Graph via import_meta_graph:
+        #   - construct protocol buffer from file content
+        #   - add all nodes to current graph and recreate collections
+        #   - return Saver
+        saver = tf.train.import_meta_graph(latest_checkpoint + '.meta') #returns the saver object
+        #restore model into session
+        saver.restore(self.session, latest_checkpoint)
+        graph = tf.get_default_graph()
+        #restore netDict manually because had to be deleted before pickling the object
+        self.netDict = {}
+        self.netDict["in"] = graph.get_tensor_by_name("nn/inputLayer:0")
+        self.netDict["out"] = graph.get_tensor_by_name("nn/out/BiasAdd:0")
+        self.netDict["target"] = graph.get_tensor_by_name("optimizer/target:0")
+        self.netDict["score"] = graph.get_tensor_by_name("summaries/score:0")
+        self.netDict["epsilon"] = graph.get_tensor_by_name("summaries/epsilon:0")
+        self.netDict["episodes"] = graph.get_tensor_by_name("summaries/episodes:0")
+        self.netDict["optimizer"] = graph.get_operation_by_name("optimizer/adamOpt")
+        self.netDict["learningRate"] = graph.get_tensor_by_name("optimizer/learningRate:0")
+        self.netDict["summaryWriter"] = tf.summary.FileWriter(self.tensorboardDir+self.runNum)
+        self.netDict["summary"] = graph.get_tensor_by_name("Merge/MergeSummary:0")#tf.summary.merge_all()
+        self.netDict["saver"] = saver
+        print("Restored")
 
     def action(self, observations):
         """
@@ -87,7 +160,7 @@ class NNAgent:
             actionVals = self.session.run(self.netDict["out"],
                              feed_dict={self.netDict["in"]: np.reshape(observations, (1,4))})
             action = np.argmax(actionVals)
-            print(actionVals, action)
+            #print(actionVals, action)
         self.steps+=1
         return action
 
@@ -133,6 +206,11 @@ class NNAgent:
         """
         self.epsilon *= self.epsilonDecay
         self.episodes+=1
+
+    def kill(self):
+        self.save() #session is closed in save
+        tf.reset_default_graph() #clean up
+        #self.reset_default_graph()
 
     def test(self):
         x = np.reshape(np.array([1,2,3,4]), (1,4))
