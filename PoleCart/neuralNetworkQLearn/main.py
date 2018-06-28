8749d9f1418e6fc96c9e5e93d34e7d45a40146fd
thosken13 <tristanhosken@talktalk.net>
2018-06-28 12:46:25 +0100

    Moved around running of epsidoe(needs sorting better) and allow a validation run where a score can be found without the random action choices from epsilon greedy policy

--- PoleCart/neuralNetworkQLearn/main.py
+++ PoleCart/neuralNetworkQLearn/main.py
@@ -21,41 +21,60 @@ def testAndExperiment():
     agent.testNpSeed()
     print(agent.action([1,2,3,4]))
 
-def runEpisodes(numEpisodes, environment, agent, train, render, saveFreq):
+def getRefObs():
+    env = gym.make('CartPole-v0')
+    env.seed(0)
+    env.reset()
+    d=False
+    while not d:
+        obs, r, d, i = env.step(env.action_space.sample())
+        print(obs)
+
+def runEpisode(environment, agent, train, render):
+    done=False
+    score=0
+    obs = environment.reset()
+    agent.prevState = np.array(obs)
+    while not done:
+        action = agent.action(obs)
+        obs, reward, done, info = environment.step(action)
+        if train:
+            agent.update(obs, action, reward, done)
+        if render:
+            environment.render()
+        if not done:
+            score+=1
+    return score
+
+def runEpisodes(numEpisodes, environment, agent, train, validate, render, saveFreq):
     for e in range(numEpisodes):
-        done=False
-        score=0
-        obs = environment.reset()
-        agent.prevState = np.array(obs)
-        while not done:
-            action = agent.action(obs)
-            obs, reward, done, info = environment.step(action)
-            if train:
-                agent.update(obs, action, reward, done)
-            if render:
-                environment.render()
-            if not done:
-                score+=1
+        runEpisode(environment, agent, train, render)
+        if validate:
+            epsilon = agent.epsilon
+            agent.epsilon = 0
+            score = runEpisode(environment, agent, False, False)
+            agent.epsilon = epsilon
+            agent.score=score
         if saveFreq and (e+1)%saveFreq==0: #check saveFreq not None (no saving)
             agent.save()
             agent.restore()
-        agent.score=score
         agent.reset()
 
 def playAndTrain(numEpisodes, saveFreq=10, render=False, nHidd=[10,10,10]):
     env = gym.make('CartPole-v0')
     env.seed(0)
-    agent = nnAgent.NNAgent(env, nNeuronsHidLayers=nHidd)
-    runEpisodes(numEpisodes, env, agent, True, render, saveFreq)
+    agent = nnAgent.NNAgent(env, nNeuronsHidLayers=nHidd, alpha=0.005, epsilonDecay=0.99, batchSize=16)
+    runEpisodes(numEpisodes, env, agent, True, True, render, saveFreq)
     agent.kill()
 
 def loopArchitecture():
     for layers in range(1,6):
         for nodesPerLayer in range(2,15):
-            playAndTrain(250, saveFreq=None, nHidd=[nodesPerLayer]*layers)
+            playAndTrain(400, saveFreq=None, nHidd=[nodesPerLayer]*layers)
 
 
 #testAndExperiment()
-#playAndTrain(100)
-#playAndTrain(100, saveFreq=None)
-loopArchitecture()
+#getRefObs()
+#playAndTrain(300)
+playAndTrain(300, saveFreq=None)
+#loopArchitecture()
